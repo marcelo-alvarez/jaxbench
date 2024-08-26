14cdf67fa45dcf06c7ba786ad2e14eea3388e6b6
Marcelo Alvarez <malvarez@email>
2024-08-26 12:47:51 -0700

    do cpu

--- fp3264.py
+++ fp3264.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 import jax
 import numpy as np
+import jax.numpy as jnp
 import scipy
 from jax import jit
 from jax.experimental import mesh_utils
@@ -15,6 +16,7 @@ from pario import parprint
 nr = 5
 nt = 3
 verbose = False
+docpu = False
 size2 = 1024
 if len(sys.argv) > 1: 
     size2 = int(sys.argv[1])
@@ -26,8 +28,11 @@ num_gpus = jax.device_count()
 
 global_shape = (size, size, size)
 rng = np.random.default_rng(12345)
-x_np_32 = rng.random(size, dtype=np.float32)
-x_np_64 = rng.random(size, dtype=np.float64)
+x_np_gpu_32 = jnp.asarray(rng.random(size, dtype=np.float32))
+x_np_gpu_64 = jnp.asarray(rng.random(size, dtype=np.float64))
+if docpu:
+    x_np_cpu_32 = rng.random(size, dtype=np.float32)
+    x_np_cpu_64 = rng.random(size, dtype=np.float64)
 
 if verbose:
     parprint("\n----------------------")
@@ -41,15 +46,22 @@ if verbose:
 
 polytest = lambda x: 2*x - 3*x**2 + 5*x**3 - 7*x**4
 
-# warmup 
-y_np_32 = polytest(x_np_32)
-y_np_64 = polytest(x_np_64)
+# warmup
+y_np_gpu_32 = polytest(x_np_gpu_32)
+y_np_gpu_64 = polytest(x_np_gpu_64)
 
 # measure
-dt_32 = np.asarray(timeit.Timer(lambda: polytest(x_np_32)).repeat(repeat=nr,number=nt))
-dt_64 = np.asarray(timeit.Timer(lambda: polytest(x_np_64)).repeat(repeat=nr,number=nt))
+dt_gpu_32 = np.asarray(timeit.Timer(lambda: polytest(x_np_gpu_32)).repeat(repeat=nr,number=nt)).mean()
+dt_gpu_64 = np.asarray(timeit.Timer(lambda: polytest(x_np_gpu_64)).repeat(repeat=nr,number=nt)).mean()
+if docpu:
+    dt_cpu_32 = np.asarray(timeit.Timer(lambda: polytest(x_np_cpu_32)).repeat(repeat=nr,number=nt)).mean()
+    dt_cpu_64 = np.asarray(timeit.Timer(lambda: polytest(x_np_cpu_64)).repeat(repeat=nr,number=nt)).mean()
 
 parprint()
-parprint(f"32-bit float times: ",dt_32)
-parprint(f"64-bit float times: ",dt_64)
+if docpu:
+    parprint(f"32-bit CPU float time: {dt_cpu_32:.2f}")
+    parprint(f"64-bit CPU float time: {dt_cpu_64:.2f}")
+parprint(f"32-bit GPU float time: {dt_gpu_32:.2e}")
+parprint(f"64-bit GPU float time: {dt_gpu_64:.2e}")
+parprint(f"ratio: {dt_gpu_64/dt_gpu_32:.2f}")
 parprint()
